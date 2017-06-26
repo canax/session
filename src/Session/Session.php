ac7b95fe9433430ed2055d3699d80aa355b8a61f
Mikael Roos <mikael.t.h.roos@gmail.com>
2017-06-26 14:21:47 +0200

    Added method to destroy session.

--- src/Session/Session.php
+++ src/Session/Session.php
@@ -123,4 +123,36 @@ class Session
         unset($_SESSION[$key]);
         return $this;
     }
+
+
+
+    /**
+     * Destroy the session.
+     *
+     * @return void
+     *
+     * @SuppressWarnings(PHPMD.Superglobals)
+     */
+    public function destroy()
+    {
+        // Unset all of the session variables.
+        $_SESSION = array();
+
+        // Delete the session cookie.
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(
+                session_name(),
+                '',
+                time() - 42000,
+                $params["path"],
+                $params["domain"],
+                $params["secure"],
+                $params["httponly"]
+            );
+        }
+
+        // Finally, destroy the session.
+        session_destroy();
+    }
 }
