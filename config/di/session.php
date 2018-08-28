086da150f17ae807921186956bfe6013637ed82d
Mikael Roos <mikael.t.h.roos@gmail.com>
2018-08-28 19:28:29 +0200

    Use constant() to check for ANAX_WITH_SESSION in di config.

--- config/di/session.php
+++ config/di/session.php
@@ -6,7 +6,7 @@ return [
     // Services to add to the container.
     "services" => [
         "session" => [
-            "active" => defined("ANAX_WITH_SESSION") && ANAX_WITH_SESSION, // true|false
+            "active" => constant("ANAX_WITH_SESSION") // true|false
             "shared" => true,
             "callback" => function () {
                 $session = new \Anax\Session\Session();
